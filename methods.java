ef27a616a26ef2ed952d499060d9e40db750fdf8
java <kverneet01@gmail.com>
2023-09-27 00:11:19 +0530

    changed

---
+++ methods.java
@@ -0,0 +1,18 @@
+public class methods {
+    
+    static int max(int x, int y){
+        x++;
+        if(x>y){
+            return x;
+        }
+        else{
+            return y;
+        }
+    }
+    public static void main(String args[]){
+      int a = 10 , b=15 , c;
+      c = max(a,b);
+      System.out.println(c);
+
+    }
+}
